8a4712d7258155382c0d42366d63b1ac81f0a8be
jsmith <jsmith@f827c759-226e-4a48-a6ac-82325153066a>
2010-09-07 21:01:38 +0000

    Branch commit work, basic layout
    git-svn-id: https://svn.devjason.com/projects/paisley/Paisley@866 f827c759-226e-4a48-a6ac-82325153066a

--- grails-app/controllers/com/devjason/paisley/DashboardController.groovy
+++ grails-app/controllers/com/devjason/paisley/DashboardController.groovy
@@ -40,4 +40,66 @@ class DashboardController {
 		map.table = table
 		render "google.visualization.Query.setResponse(" + (map as JSON) + ")"
 	}
+	
+	
+	
+	def monthly_commits_by_branch = {
+		def db = new Sql(dataSource)
+		
+		// Setup Google Response object
+		def map = [:]
+		// map.version = 0.5
+		map.reqId = '1'
+		map.status = 'ok'
+		
+		// Query for active branches
+		def results = db.rows(
+			"select distinct b.name " +
+			"from branch b " +
+			"join revision_entry re on b.id = re.branch_id " +
+			"join revision r on re.revision_id = r.id " +
+			"where r.revision_time > DATE_SUB(CURDATE(), INTERVAL 6 WEEK) " +
+			"order by b.name asc"
+		)
+		def branches = results.collect { it.name }
+		def columns = []
+		columns << [label: 'Month', type: 'string']
+		branches.each {
+			columns << [label: it, type: 'number']
+		}
+		
+		// Query for commit counts
+		results = db.rows(
+			"select year(revision_time) as yr, " +
+			"month(revision_time) as mth, " +
+			"day(revision_time) as day, " +
+			"count(*) as cnt, " +
+			"b.name as name " +
+			"from revision " +
+			"join revision_entry re on re.revision_id = revision.id " +
+			"join branch b on re.branch_id = b.id " +
+			"where revision_time > DATE_SUB(CURDATE(), INTERVAL 6 WEEK) " +
+			"group by year(revision_time), month(revision_time), day(revision_time), b.name"
+		)
+		def min = {a,b -> a < b ? a : b }
+		def rows = []
+		def cells
+		results.each {
+			cells = []
+			cells << [v: "${it.day}-${it.mth}-${it.yr}"]
+			branches.each { b ->
+				if (b.equalsIgnoreCase(it.name)) {
+					cells << [v: it.cnt]
+				} else {
+					cells << [v: 0]
+				}
+			}
+			rows << ['c': cells]
+		}
+		
+		def table = [cols: columns, rows: rows]
+		map.table = table
+		render "google.visualization.Query.setResponse(" + (map as JSON) + ")"
+	}
+	
 }
--- grails-app/views/dashboard/index.gsp
+++ grails-app/views/dashboard/index.gsp
@@ -10,13 +10,16 @@
 		function initialize() {
 			var query = new google.visualization.Query("${createLink(action:'monthly_commits')}");
 			query.send(drawBarChart);
+
+			var query2 = new google.visualization.Query("${createLink(action:'monthly_commits_by_branch')}");
+			query2.send(drawBranchChart);
 		}
 		function drawBarChart(response) {			
 			var data = response.getDataTable();
 			var chart = new google.visualization.ColumnChart(
 				document.getElementById('chart_div'));
 			var chartOptions = {
-					width:1000, height:300, legend:'none', title:'Monthly Commit Volume',
+					legend:'none', title:'Monthly Commit Volume',
 					backgroundColor:'black',
 					titleTextStyle:{color:'yellow'},
 					hAxis:{textStyle:{color:'white'}},
@@ -24,10 +27,27 @@
 			};
 			chart.draw(data, chartOptions);
 		};
+		
+		function drawBranchChart(response) {			
+			var data = response.getDataTable();
+			var chart = new google.visualization.ColumnChart(
+				document.getElementById('branch_commits_div'));
+			var chartOptions = {
+					title:'Branch Commits Last Six Weeks',
+					backgroundColor:'black',
+					titleTextStyle:{color:'yellow'},
+					hAxis:{textStyle:{color:'white'}},
+					vAxis:{textStyle:{color:'white'}, baselineColor:'white', logScale: true},
+					legendTextStyle:{color:'white', fontSize:8},
+					isStacked:true,
+			};
+			chart.draw(data, chartOptions);
+		};
 	</script>
 </head>
 <body>
-	<h1>Paisley FCE Analytics</h1>
-	<div id="chart_div"></div>
+	<h1>FCE SVN Analytics</h1>
+	<div id="chart_div" class="chart"></div>
+	<div id="branch_commits_div" class="chart"></div>
 </body>
 </html>
