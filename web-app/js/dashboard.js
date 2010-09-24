98292702a55ecfbdbb90dbe57715c42ee3024353
bdangubic <bdangubic@f827c759-226e-4a48-a6ac-82325153066a>
2010-09-24 12:02:47 +0000

    handle other JIRA prefixes (WBG, FCA, CP)...
    git-svn-id: https://svn.devjason.com/projects/paisley/Paisley@893 f827c759-226e-4a48-a6ac-82325153066a

--- web-app/js/dashboard.js
+++ web-app/js/dashboard.js
@@ -2,10 +2,6 @@ var DASHBOARD = {};
 
 DASHBOARD.fceJIRALinks = function() {
 	$('.google-visualization-table-table tr td:last-child').each(function() {
-		var jiraPrefix = "<a href='http://jira.justicesystems.com/jira/browse/";
-		var jiraSuffix = "' class='jiraLink' target='_window'>";
-		var jiraEnd = "</a>";
-						
 		var svnMessage = $(this).text(); 
 
 		//
@@ -23,56 +19,65 @@ DASHBOARD.fceJIRALinks = function() {
 			$(this).html(beforeLink + "<a href='" + link + "' class='jiraLink' target='_window'>" + link.substring(link.lastIndexOf("/")+1) + " </a> " + afterLink);  
 		}
 		//
-		// FCE is somewhere in the middle of the message but no link
-		else if (svnMessage.indexOf("FCE-") != -1) {
-			var svnFCEPartToWrap = '';
-			var svnNonFCEBefore = svnMessage.substring(0,svnMessage.indexOf("FCE-"));
-			//
-			// message starts with JIRA number
-			if (svnNonFCEBefore == '') {						
-				if (svnMessage.indexOf(' ') == -1) {
-					// 
-					// nothing in the message but JIRA number
-					$(this).html(jiraPrefix + svnMessage + jiraSuffix + svnMessage + jiraEnd);
-				} else {
-					//
-					// JIRA number plus additional text after the JIRA number
-					var jiraNumber = svnMessage.substring(0,svnMessage.indexOf(' '));
-					$(this).html(jiraPrefix + jiraNumber.replace(':','') + jiraSuffix + jiraNumber + jiraEnd + svnMessage.substring(svnMessage.indexOf(' ')+1));
-				}
+		// JIRA number is somewhere in the middle of the message but no link
+		$element = $(this);
+		$.each(['FCE-', 'WBG-', 'CP-', 'FCA-'], function(index, value) {
+			if (svnMessage.indexOf(value) != -1) {
+				DASHBOARD.addLink($element, svnMessage,value);
 			}
-			// message contains JIRA number somewhere 
-			else {						
-				var svnMessageAfter = svnMessage.substring(svnNonFCEBefore.length-1);						
-				if (svnMessageAfter.indexOf(' ') == -1) {
-					alert('setting this to be html, nothing in the string except JIRA # after: ' +
-							svnNonFCEBefore + jiraPrefix + svnMessageAfter + jiraSuffix + svnMessageAfter + jiraEnd);
-					$(this).html(svnNonFCEBefore + jiraPrefix + svnMessageAfter + jiraSuffix + svnMessageAfter + jiraEnd);
-				} else {
-					//
-					// JIRA number plus additional text after the JIRA number
-					svnMessageAfter = svnMessageAfter.substring(1);
-					var jiraNumber = svnMessageAfter.substring(0,svnMessageAfter.indexOf(' '));
-					if (jiraNumber == '') {
-						jiraNumber = svnMessageAfter;
-						svnMessageAfter = '';
-					}
-					$(this).html(svnNonFCEBefore + 
-							jiraPrefix + 
-							jiraNumber.replace(':','') + 
-							jiraSuffix + 
-							jiraNumber + 
-							jiraEnd + 
-							svnMessageAfter.substring(svnMessageAfter.indexOf(' ')+1));
-				}
-			}
-		}  
-	});	
+		})		
+	});		
 }
 
-jQuery(document).ready(function() {
-	setTimeout(DASHBOARD.fceJIRALinks, 500);
+DASHBOARD.addLink = function($element, svnMessage, prefix) {
+	var jiraPrefix = "<a href='http://jira.justicesystems.com/jira/browse/";
+	var jiraSuffix = "' class='jiraLink' target='_window'>";
+	var jiraEnd = "</a>";
+	
+	var svnFCEPartToWrap = '';
+	var svnNonFCEBefore = svnMessage.substring(0,svnMessage.indexOf(prefix));
+	//
+	// message starts with JIRA number
+	if (svnNonFCEBefore == '') {						
+		if (svnMessage.indexOf(' ') == -1) {
+			// 
+			// nothing in the message but JIRA number
+			$element.html(jiraPrefix + svnMessage + jiraSuffix + svnMessage + jiraEnd);
+		} else {
+			//
+			// JIRA number plus additional text after the JIRA number
+			var jiraNumber = svnMessage.substring(0,svnMessage.indexOf(' '));
+			$element.html(jiraPrefix + jiraNumber.replace(':','') + jiraSuffix + jiraNumber + jiraEnd + svnMessage.substring(svnMessage.indexOf(' ')+1));
+		}
+	}
+	// message contains JIRA number somewhere 
+	else {						
+		var svnMessageAfter = svnMessage.substring(svnNonFCEBefore.length-1);						
+		if (svnMessageAfter.indexOf(' ') == -1) {
+			$element.html(svnNonFCEBefore + jiraPrefix + svnMessageAfter + jiraSuffix + svnMessageAfter + jiraEnd);
+		} else {
+			//
+			// JIRA number plus additional text after the JIRA number
+			svnMessageAfter = svnMessageAfter.substring(1);
+			var jiraNumber = svnMessageAfter.substring(0,svnMessageAfter.indexOf(' '));
+			if (jiraNumber == '') {
+				jiraNumber = svnMessageAfter;
+				svnMessageAfter = '';
+			}
+			$element.html(svnNonFCEBefore + 
+					jiraPrefix + 
+					jiraNumber.replace(':','') + 
+					jiraSuffix + 
+					jiraNumber + 
+					jiraEnd + 
+					svnMessageAfter.substring(svnMessageAfter.indexOf(' ')+1));
+		}
+	}
+}
 
+jQuery(function() {
+	setTimeout(DASHBOARD.fceJIRALinks, 500);
+	
 	/*
 	jQuery('tr.dtHoverRow').live('hover', function() {
 	}, function() {
